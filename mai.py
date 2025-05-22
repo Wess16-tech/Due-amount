bad7d1200fe94ba75076e949a4e64163bcc20ea6
NatalieZawadi <nataliezawadiwesula@gmail.com>
2025-05-22 20:25:37 +0300

    due amount

--- mai.py
+++ mai.py
@@ -1,15 +1,15 @@
 total = 100
-print("Your total is", total)
+print("Your due amount is", total)
 payed_amount = int(input("Enter the amount you want to pay: "))
 
 def total_new(total, payed_amount):
     total = total - payed_amount
-    print("Your due amount is", total)
+    print("Your new due amount is", total)
 
 if payed_amount == total:
     print("Your payment is complete")
 elif payed_amount > total:
-    print("Your payment is more than the total amount")
+    print("Your payment is more than the due amount")
     total_new(total, payed_amount)
 else:
     total_new(total, payed_amount)
